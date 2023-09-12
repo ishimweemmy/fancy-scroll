84ce8e0131063f7308a6cb144731a88bc46c7c6f
ishimwe-emma <ishimweemmanuel2005@gmail.com>
2023-09-12 21:14:04 +0200

    [feat]: completed the important logic

---
+++ .history/index_20230912161934.js
@@ -0,0 +1,20 @@
+const canvas = document.querySelector("canvas");
+const currentFrame = (index) => {
+  return `images/${index.toString().padStart(4, "0")}.png`;
+};
+const html = document.querySelector("html");
+
+window.addEventListener("scroll", (e) => {
+  const scrollTop = html.scrollTop;
+  const maxScrollTop = html.scrollHeight - window.innerHeight;
+  const percentage = Math.random((scrollTop * 100) / maxScrollTop);
+  console.log(percentage)
+  requestAnimationFrame(() => updateImage(percentage));
+});
+
+const updateImage = (index) => {
+  const context = canvas.getContext("2d");
+  const img = document.querySelector("img");
+  img.src = currentFrame(index);
+  context.drawImage(img, 0, 0);
+};
